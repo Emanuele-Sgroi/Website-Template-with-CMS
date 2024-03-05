3c657c5c4821d8443a455118a4c03f3ad8a4f281
Emanuele Sgroi <nunzioemanuelesgroi@gmail.com>
2024-03-05 16:48:54 +0000

    refactor(pages): central routing export

--- frontend_react/src/pages/index.js
+++ frontend_react/src/pages/index.js
@@ -1,3 +1,4 @@
 export { default as Home } from "./home/Home";
 export { default as ServiceDetails } from "./serviceDetails/ServiceDetails";
 export { default as NotFound } from "./notFound/NotFound";
+//
